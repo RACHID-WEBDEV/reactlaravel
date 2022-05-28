799d8ecbc45780a514676d1c053306c3f87a152d
github.com/RACHID-WEBDEV <rasheedolamide2015@gmail.com>
2022-05-28 23:38:58 +0100

    add Delete modal component

---
+++ resources/js/components/studentLists/Modals/DeleteModal.js
@@ -0,0 +1,73 @@
+import axios from "axios";
+import React, { Component } from "react";
+import { toast } from "react-toastify";
+
+class DeleteModal extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    // Delete function for Student data.
+
+    deleteStudentData = (student) => {
+        axios.delete("/delete/student/data/" + student).then(() => {
+            toast.error("Student Deleted successfully");
+
+            setTimeout(() => {
+                location.reload();
+            }, 2500);
+        });
+    };
+
+    render() {
+        return (
+            <div
+                className="modal fade"
+                id={"deleteModal" + this.props.modalId}
+                tabIndex="-1"
+                aria-labelledby="exampleModalLabel"
+                aria-hidden="true"
+            >
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">
+                                Student Delete
+                            </h5>
+                            <button
+                                type="button"
+                                className="btn-close"
+                                data-bs-dismiss="modal"
+                                aria-label="Close"
+                            ></button>
+                        </div>
+                        <div className="modal-body">
+                            Are you sure, You want to delete this Student data
+                        </div>
+                        <div className="modal-footer">
+                            <button
+                                type="button"
+                                className="btn btn-danger"
+                                data-bs-dismiss="modal"
+                                onClick={() => {
+                                    this.deleteStudentData(this.props.modalId);
+                                }}
+                            >
+                                Yes
+                            </button>
+                            <button
+                                type="button"
+                                className="btn btn-secondary"
+                                data-bs-dismiss="modal"
+                            >
+                                Close
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default DeleteModal;
